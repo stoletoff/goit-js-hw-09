06d7a396220ec120e9cd8159cf5704f63ff39e96
Alex <afanasbev1@gmail.com>
2023-04-02 16:23:38 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+    timerId = setInterval(setBodyBG, 1000);
+}
+function setBodyBG() {
+    document.body.style.backgroundColor = getRandomHexColor();
+    startBtn.setAttribute("disabled", "disabled");
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+
+function onStopBtnClick(){
+    clearInterval(timerId);
+    startBtn.removeAttribute("disabled");
+}
